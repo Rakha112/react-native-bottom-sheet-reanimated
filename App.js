5209276445e5643212996f902440439fe72f3d2d
Rakha112 <rakhawibowo1998@gmail.com>
2022-08-28 11:02:21 +0700

    Create Bottom Sheet

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import {
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  useWindowDimensions,
+} from 'react-native';
+import React, {useRef, useCallback} from 'react';
+import BottomSheet from './src/components/BottomSheet';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
+  const {height} = useWindowDimensions();
+  const bottomSheetRef = useRef();
+  const pressHandler = useCallback(() => {
+    bottomSheetRef.current.expand();
+  }, []);
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaView style={styles.container}>
+        <Button title="Open Bottom Sheet" onPress={() => pressHandler()} />
+        <BottomSheet activeHeight={height * 0.6} ref={bottomSheetRef} />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
---
+++ src/components/BottomSheet.js
@@ -0,0 +1,154 @@
+import {
+  StyleSheet,
+  View,
+  useWindowDimensions,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import React, {
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useCallback,
+} from 'react';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  useAnimatedGestureHandler,
+  interpolate,
+} from 'react-native-reanimated';
+import {PanGestureHandler} from 'react-native-gesture-handler';
+const BottomSheet = forwardRef(({activeHeight}, ref) => {
+  const {height} = useWindowDimensions();
+  const [newActiveHeight] = useState(height - activeHeight);
+  const topAnimation = useSharedValue(height);
+
+  const expand = useCallback(() => {
+    'worklet';
+    topAnimation.value = withSpring(newActiveHeight, {
+      damping: 100,
+      stiffness: 400,
+    });
+  }, [newActiveHeight, topAnimation]);
+
+  const close = useCallback(() => {
+    'worklet';
+    topAnimation.value = withSpring(height, {
+      damping: 100,
+      stiffness: 400,
+    });
+  }, [height, topAnimation]);
+
+  const backDropPressHandler = useCallback(() => {
+    'worklet';
+    topAnimation.value = withSpring(height, {
+      damping: 100,
+      stiffness: 400,
+    });
+  }, [height, topAnimation]);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      expand,
+      close,
+    }),
+    [expand, close],
+  );
+
+  const animationStyle = useAnimatedStyle(() => {
+    const top = topAnimation.value;
+    return {
+      top,
+    };
+  });
+  const backDropAnimation = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      topAnimation.value,
+      [height, newActiveHeight],
+      [0, 0.5],
+    );
+    const display = opacity === 0 ? 'none' : 'flex';
+    return {
+      opacity,
+      display,
+    };
+  });
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = topAnimation.value;
+    },
+    onActive: (event, ctx) => {
+      if (event.translationY < 0) {
+        topAnimation.value = newActiveHeight;
+      } else {
+        topAnimation.value = ctx.startX + event.translationY;
+      }
+    },
+    onEnd: _ => {
+      if (topAnimation.value > newActiveHeight + 50) {
+        topAnimation.value = withSpring(height, {
+          damping: 100,
+          stiffness: 400,
+        });
+      } else {
+        topAnimation.value = withSpring(newActiveHeight, {
+          damping: 100,
+          stiffness: 400,
+        });
+      }
+    },
+  });
+
+  return (
+    <>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          backDropPressHandler();
+        }}>
+        <Animated.View style={[styles.backDrop, backDropAnimation]} />
+      </TouchableWithoutFeedback>
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.container, animationStyle]}>
+          <View style={styles.lineContainer}>
+            <View style={styles.line} />
+          </View>
+        </Animated.View>
+      </PanGestureHandler>
+    </>
+  );
+});
+
+export default BottomSheet;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  lineContainer: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  line: {
+    width: 50,
+    height: 4,
+    backgroundColor: 'black',
+    borderRadius: 20,
+  },
+  backDrop: {
+    backgroundColor: 'black',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: 'none',
+  },
+});
