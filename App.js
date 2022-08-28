8fa333bc66705fdfbac911504f64d06e8b255aa6
Rakha112 <rakhawibowo1998@gmail.com>
2022-08-28 19:11:02 +0700

    Add example

--- App.js
+++ App.js
@@ -16,20 +16,31 @@ const App = () => {
   const {height} = useWindowDimensions();
   const bottomSheetRef = useRef();
   const bottomSheetRef2 = useRef();
+  const bottomSheetRef3 = useRef();
   const pressHandler = useCallback(() => {
     bottomSheetRef.current.expand();
   }, []);
   const pressHandler2 = useCallback(() => {
     bottomSheetRef2.current.expand();
   }, []);
+  const pressHandler3 = useCallback(() => {
+    bottomSheetRef3.current.expand();
+  }, []);
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaView style={styles.container}>
-        <Button title="Example 1" onPress={() => pressHandler()} />
-        <Button title="Blank" onPress={() => pressHandler2()} />
+        <Button title="Blank" onPress={() => pressHandler()} />
+        <Button title="Example 1" onPress={() => pressHandler2()} />
+        <Button title="Example 2" onPress={() => pressHandler3()} />
         <BottomSheet
           ref={bottomSheetRef}
+          activeHeight={height * 0.5}
+          backgroundColor={'white'}
+          backDropColor={'black'}
+        />
+        <BottomSheet
+          ref={bottomSheetRef2}
           activeHeight={height * 0.5}
           backgroundColor={'#DAD3C8'}
           backDropColor={'black'}>
@@ -59,11 +70,28 @@ const App = () => {
           </View>
         </BottomSheet>
         <BottomSheet
-          ref={bottomSheetRef2}
+          ref={bottomSheetRef3}
           activeHeight={height * 0.5}
-          backgroundColor={'white'}
-          backDropColor={'black'}
-        />
+          backgroundColor={'#FFFFFF'}
+          backDropColor={'black'}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'space-between',
+            }}>
+            <View style={styles.textContainer}>
+              <Text style={styles.textExample2}>Good Evening</Text>
+              <Text style={styles.textExample2}>Everyday is a good day</Text>
+            </View>
+            <View style={styles.imageContaierExample2}>
+              <Text style={styles.textExample2}>Recommend</Text>
+              <Image
+                source={require('./src/assets/image2.png')}
+                style={styles.imageExample2}
+              />
+            </View>
+          </View>
+        </BottomSheet>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
@@ -81,10 +109,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  imageContaierExample2: {
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
   image: {
     width: 200,
     height: undefined,
     aspectRatio: 1,
+  },
+  imageExample2: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1.65636588,
   },
   button: {
     alignItems: 'center',
@@ -103,6 +140,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#000000',
     fontSize: 16,
+  },
+  textExample2: {
+    color: '#000000',
+    fontSize: 26,
   },
   textPrice: {
     color: '#000000',
